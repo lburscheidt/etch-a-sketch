aee1991a8c9149c0b1e262da93320f170a9a305d
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-18 10:01:37 +0200

    feat: create grid inside container

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let container = document.querySelector("#container");
-containerHeight = "500px";
-container.style.height = containerHeight;
-container.style.width = containerHeight;
+let display = document.querySelector("#display");
+displayHeight = "500px";
+display.style.height = displayHeight;
+display.style.width = displayHeight;
 
-for (let i = 1; i <= 16; i++) {
-  let square = document.createElement("div");
-  square.setAttribute("id", `square-${i}`);
-  square.setAttribute("class", "square");
-  squareHeight = 500 / 16  + "px";
-  console.log(squareHeight);
-  square.style.height = squareHeight;
-  console.log();
-  square.style.width = squareHeight;
-  container.appendChild(square);
+for (let j = 1; j <= 16; j++) {
+  for (let i = 1; i <= 16; i++) {
+    let square = document.createElement("div");
+    square.setAttribute("id", `square-${i}${j}`);
+    square.setAttribute("class", "square");
+    squareHeight = 500 / 16 + "px";
+    console.log(squareHeight);
+    square.style.height = squareHeight;
+    console.log();
+    square.style.width = squareHeight;
+    display.appendChild(square);
+  }
 }
