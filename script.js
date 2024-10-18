ed290e67edc436b98558543836e7632038bf29a3
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-18 10:41:04 +0200

    feat: change color of squares to black on mouseenter

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ displayHeight = "500px";
 display.style.height = displayHeight;
 display.style.width = displayHeight;
 
+function changeColor(element) {
+  element.style.backgroundColor = "#000";
+}
+
 for (let j = 1; j <= 16; j++) {
   for (let i = 1; i <= 16; i++) {
     let square = document.createElement("div");
     square.setAttribute("id", `square-${i}${j}`);
     square.setAttribute("class", "square");
     squareHeight = 500 / 16 + "px";
-    console.log(squareHeight);
     square.style.height = squareHeight;
-    console.log();
     square.style.width = squareHeight;
+    square.addEventListener("mouseenter", function () {
+      changeColor(square);
+    });
     display.appendChild(square);
   }
 }
