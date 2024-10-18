29d17700c06eb7b7f7311f869dfc0285c4050396
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-18 10:54:11 +0200

    feat: put code to create grid inside a function

--- script.js
+++ script.js
@@ -2,22 +2,29 @@ let display = document.querySelector("#display");
 displayHeight = "500px";
 display.style.height = displayHeight;
 display.style.width = displayHeight;
+displaySize = parseInt(displayHeight);
+console.log(displaySize);
 
 function changeColor(element) {
   element.style.backgroundColor = "#000";
 }
 
-for (let j = 1; j <= 16; j++) {
-  for (let i = 1; i <= 16; i++) {
-    let square = document.createElement("div");
-    square.setAttribute("id", `square-${i}${j}`);
-    square.setAttribute("class", "square");
-    squareHeight = 500 / 16 + "px";
-    square.style.height = squareHeight;
-    square.style.width = squareHeight;
-    square.addEventListener("mouseenter", function () {
-      changeColor(square);
-    });
-    display.appendChild(square);
+function createGrid(size = 16) {
+  for (let j = 1; j <= size; j++) {
+    for (let i = 1; i <= size; i++) {
+      let square = document.createElement("div");
+      square.setAttribute("id", `square-${i}${j}`);
+      square.setAttribute("class", "square");
+      squareHeight = displaySize / size + "px";
+      console.log(squareHeight);
+      square.style.height = squareHeight;
+      square.style.width = squareHeight;
+      square.addEventListener("mouseenter", function () {
+        changeColor(square);
+      });
+      display.appendChild(square);
+    }
   }
 }
+
+createGrid();
